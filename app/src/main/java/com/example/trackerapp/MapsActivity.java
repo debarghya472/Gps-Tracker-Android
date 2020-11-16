35a194f127e8eeebf72bdf6c2fea1d6660f5586d
debarghya472 <debarghya_1901me19@iitp.ac.in>
2020-11-16 14:02:25 +0530

    complete direction api integration

--- app/src/main/java/com/example/trackerapp/MapsActivity.java
+++ app/src/main/java/com/example/trackerapp/MapsActivity.java
@@ -18,6 +18,11 @@ import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.directions.route.AbstractRouting;
+import com.directions.route.Route;
+import com.directions.route.RouteException;
+import com.directions.route.Routing;
+import com.directions.route.RoutingListener;
 import com.example.trackerapp.network.DirectionResponse;
 import com.example.trackerapp.network.JsonPlaceHolderApi;
 import com.example.trackerapp.network.POLYline;
@@ -32,6 +37,7 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResponse;
 import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -48,6 +54,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -55,14 +62,14 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnMapLongClickListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnMapLongClickListener, RoutingListener {
 
     private GoogleMap mMap;
     private FusedLocationProviderClient client;
     private LocationRequest locationRequest;
     private Marker marker;
     private JsonPlaceHolderApi jsonPlaceHolderApi;
-    private Polyline polyline;
+    private List<Polyline> polylines=null;
 
     private Geocoder geocoder;
     private double Lat;
@@ -238,28 +245,76 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //                .color(Color.BLUE)
 //                .geodesic(true)
 //                );
-                Retrofit retrofit = RetrofitClientInstance.getRetrofitInstance();
-                jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
-
-                Call<DirectionResponse> call =jsonPlaceHolderApi.getDirection(Lat,Lon,latLng.latitude,latLng.longitude);
-                call.enqueue(new Callback<DirectionResponse>() {
-                    @Override
-                    public void onResponse(Call<DirectionResponse> call, Response<DirectionResponse> response) {
-                        if(response.isSuccessful()) {
-                            POLYline polYline = response.body().getRouteResponse().getLegsResponse().getStepResponse().getPolyline();
-                            List<String>  points= polYline.getPointsList();
-
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<DirectionResponse> call, Throwable t) {
-                        Toast.makeText(MapsActivity.this,t.getMessage(),Toast.LENGTH_SHORT).show();
-                    }
-                });
+//                Retrofit retrofit = RetrofitClientInstance.getRetrofitInstance();
+//                jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+//
+//                Call<DirectionResponse> call =jsonPlaceHolderApi.getDirection(Lat,Lon,latLng.latitude,latLng.longitude);
+//                call.enqueue(new Callback<DirectionResponse>() {
+//                    @Override
+//                    public void onResponse(Call<DirectionResponse> call, Response<DirectionResponse> response) {
+//                        if(response.isSuccessful()) {
+//                            POLYline polYline = response.body().getRouteResponse().getLegsResponse().getStepResponse().getPolyline();
+//                            List<String>  points= polYline.getPointsList();
+//
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<DirectionResponse> call, Throwable t) {
+//                        Toast.makeText(MapsActivity.this,t.getMessage(),Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+                Routing routing = new Routing.Builder()
+                        .travelMode(Routing.TravelMode.DRIVING)
+                        .withListener(this)
+                        .alternativeRoutes(true)
+                        .waypoints(new LatLng(Lat,Lon),latLng)
+                        .key("AIzaSyBdicgnEWaT0DAokMVL69Rl0jAygCdpC_M")
+                        .build();
+                routing.execute();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+
+    @Override
+    public void onRoutingFailure(RouteException e) {
+        Toast.makeText(MapsActivity.this,"Failed to update route "+ e.getMessage(),Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onRoutingStart() {
+        Toast.makeText(MapsActivity.this,"Finding Route...",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onRoutingSuccess(ArrayList<Route> arrayList, int short1) {
+        Toast.makeText(MapsActivity.this,"Success.",Toast.LENGTH_LONG).show();
+        CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(Lat,Lon));
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(16);
+        if(polylines!=null) {
+            polylines.clear();
+        }
+        PolylineOptions polyOptions = new PolylineOptions();
+
+        for (int i = 0; i <arrayList.size(); i++) {
+
+            if (i == short1) {
+                polyOptions.color(getResources().getColor(R.color.colorAccent));
+                polyOptions.width(10);
+                polyOptions.addAll(arrayList.get(short1).getPoints());
+                Polyline polyline = mMap.addPolyline(polyOptions);
+                mMap.addPolyline(polyOptions);
+
+            }
+        }
+
+    }
+
+    @Override
+    public void onRoutingCancelled() {
+        Toast.makeText(MapsActivity.this,"Re-route",Toast.LENGTH_LONG).show();
     }
 }
